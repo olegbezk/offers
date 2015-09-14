62985271a55fc523b6026e9ca744698cfdb6d880
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2015-09-14 18:22:20 +0300

    configuring for log4j

--- src/com/spring/web/controllers/HomeController.java
+++ src/com/spring/web/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package com.spring.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
+	private static Logger LOGGER = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String showHome() {
+		LOGGER.info("Showing Home page ...................");
 		return "home";
 	}
 
